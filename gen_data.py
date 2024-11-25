ac750a6a1ff2e235085c35eff83b53cfeffb31d0
Ramond Zhou <2200011057@stu.pku.edu.cn>
2024-11-25 16:02:42 +0800

    added OnTable predicate

--- gen_data.py
+++ gen_data.py
@@ -3,8 +3,7 @@ import numpy as np
 
 def gen_data(n_blocks=8):
     img = -np.ones([n_blocks, n_blocks], dtype=int)
-    vector = np.zeros([n_blocks, n_blocks], dtype=int)
-    # permutation = np.random.permutation(n_blocks)
+    vector = np.zeros([n_blocks, n_blocks + 1], dtype=int)
     permutation = np.random.permutation(n_blocks)
     n_cols = np.random.randint(1, n_blocks)
     split_points = np.sort(np.random.choice(range(n_blocks), n_cols - 1, replace=False))
@@ -15,8 +14,9 @@ def gen_data(n_blocks=8):
     print('[debug]: generated permutation: {}, generated n_cols: {}, generated splitpoints: {}, n_block_per_col: {}, generated column index: {}, generated rol index: {}'.format(permutation, n_cols, split_points, n_blocks_per_col, col_idxs, rol_idxs))
     img[rol_idxs, col_idxs] = permutation[range(n_blocks)]
     print('[debug]: image: {}'.format(img))
-    vector[permutation[split_points], n_blocks - 1] = 1
-    vector[permutation[-1], n_blocks - 1] = 1
+    vector[permutation[split_points], -2] = 1
+    vector[permutation[-1], -2] = 1
+    vector[permutation[np.where(rol_idxs == n_blocks - 1)[0]], -1] = 1
     for i in range(n_blocks - 1):
         if col_idxs[i] == col_idxs[i + 1]:
             vector[permutation[i]][permutation[i + 1]] = 1
@@ -25,13 +25,17 @@ def gen_data(n_blocks=8):
 
 
 def vector2language(vector: np.ndarray, spliter=', '):
-    assert len(vector.shape) == 2 and vector.shape[0] == vector.shape[1]
-    print('[debug]: clear objs: {}'.format(np.where(vector[:, -1])))
-    language = spliter.join(['clear({})'.format(idx) for idx in (np.where(vector[:, -1])[0])])
+    assert len(vector.shape) == 2 and vector.shape[0] == vector.shape[1] - 1
+    N, D = vector.shape
+    print('[debug]: clear objs: {}'.format(np.where(vector[:, -2])))
+    language = spliter.join(['clear({})'.format(idx) for idx in (np.where(vector[:, -2])[0])])
     language += '\n'
     print('[debug]: language: {}'.format(language))
     language += spliter.join(['On({}, {})'.format(j, i) for i, j in zip(*np.where(vector[:, :-1]))])
+    language += '\n'
+    language += spliter.join(['OnTable({})'.format(i) for i in np.where(vector[:, -1])[0]])
     print('[debug]: language: {}'.format(language))
+
 
 
 if __name__ == '__main__':
