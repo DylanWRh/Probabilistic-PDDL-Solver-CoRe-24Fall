ee2eb8659f0fe6611364fda7c4fab17349e8b73e
Ramond Zhou <2200011057@stu.pku.edu.cn>
2025-01-08 21:22:28 +0800

    implemented continous planner and task manager for evaluating

--- planner.py
+++ planner.py
@@ -5,88 +5,170 @@ from model.sgn import BlockStackingSGN
 from environment import BlockStackingEnv
 from data_utils.dataset import BlockStackingTaskDataset
 import heapq
+import queue
 
 
-class BlockStackingPlanner:
-    def __init__(self, env: BlockStackingEnv, init_state, goal_state, model):
-        self.env = env
-        self.cur_state = init_state
-        self.goal_state = goal_state
-        self.sgn_model = model
-    
-    def coord2state_sgn(self, coords):
-        return self.sgn_model(coords)
-    
-    def set_coords(self, coords):
-        self.cur_state = self.coord2state_sgn(coords)
-    
-    def set_coords_goal(self, coords):
-        self.goal_state = self.coord2state_sgn(coords)
-    
-    def set_state(self, state):
-        self.cur_state = state
-    
-    def set_goal(self, goal):
-        self.goal_state = goal
-    
-    def get_all_actions(self):
-        actions = []
-        for i in range(1, self.env.num_blocks + 1):
-            actions.append(('Put on table', i))
-        for i in range(1, self.env.num_blocks + 1):
-            for j in range(1, self.env.num_blocks + 1):
-                if i != j:
-                    actions.append(('Put on', i, j))
-        return actions
-    
-    def goal_diff(self, state=None, matching_fn='L2'):
-        if state is None:
-            state = self.cur_state
-        assert matching_fn in ['L2', 'L1', 'cross_entropy'], \
-            f"Unknown matching function: {matching_fn}"
-        if matching_fn == 'L2':
-            return np.linalg.norm(state - self.goal_state)
-        if matching_fn == 'L1':
-            return np.linalg.norm(state - self.goal_state, ord=1)
-        if matching_fn == 'cross_entropy':
-            return -np.sum(self.goal_state * np.log(state + 1e-10))
-    
-    def goal_reached(self, state=None, EPS=0, matching_fn='L2'):
-        if state is None:
-            state = self.cur_state
-        return self.goal_diff(state, matching_fn) <= EPS
-    
-    def action_applicable(self, action):
-        if action[0] == 'Put on table':
-            return self.env.put_on_table(action[1], judge_only=True)
-        if action[0] == 'Put on':
-            return self.env.put_A_on_B(action[1], action[2], judge_only=True)
-        assert False, f'Unknown action: {action}'
-    
-    def run_straight(self, max_iter=100):
-        pass
-    
-    def run(self, max_iter=100, method='straight'):
-        
-        if method == 'straight':
-            return self.run_straight(max_iter)
-        else:
-            assert False, f'Unknown method: {method}'
-        
-        
-        
+def atom2idx(atom):
+    atom_type = atom[0]
+    if atom_type.lower() == 'clear':
+        block_idx = atom[1] - 1
+        return (block_idx, -2)
+    elif atom_type.lower() == 'ontable':
+        block_idx = atom[1] - 1
+        return (block_idx, -1)
+    elif atom_type.lower() == 'on':
+        block_a, block_b = atom[1] - 1, atom[2] - 1
+        return (block_a, block_b)
+    else:
+        raise NotImplementedError()
 
-if __name__ == '__main__':   
-    task_dataset = BlockStackingTaskDataset('./data/tasks/8blocks-3000-basic.npz')
-    model = BlockStackingSGN(8, hidden_dim=128, depth=6)
-    # model.load_state_dict(torch.load('checkpoints/model_010.pth'))
-    planner = BlockStackingPlanner(BlockStackingEnv(8), None, None, model)
-    for task in task_dataset:
-        print(task)
-        planner.set_coords(task['init']['data'])
-        planner.set_coords_goal(task['goal']['data'])
-        
-        break
-    
-    
-    
+
+def get_atom_prob(state, atom):
+    return state[atom2idx(atom)]
+
+
+def get_applicability(state, action):
+    block_a, block_b = action[1], action[2]
+    return get_atom_prob(state, ['Clear', block_a]) * get_atom_prob(state, ['Clear', block_b])
+
+
+def get_effective_set(action):
+    block_a, block_b = action[1], action[2]
+    return (
+        [
+            ['on', block_a, block_b]
+        ],
+        [
+            ['clear', block_b],
+        ]
+    )
+
+
+def apply_action(state, action):
+    new_state = state.copy()
+    positive_set, negative_set = get_effective_set(action)
+    applicability = get_applicability(state, action)
+    for positive_atom in positive_set:
+        atom_idx = atom2idx(positive_atom)
+        new_state[atom_idx] = np.clip(applicability + (1 - applicability) * state[atom_idx], 0, 1)
+    for negative_atom in negative_set:
+        atom_idx = atom2idx(negative_atom)
+        new_state[atom_idx] = np.clip(state[atom_idx] - applicability, 0, 1)
+    return new_state
+
+
+def list_action(n_blocks):
+    res = []
+    for i in range(1, n_blocks + 1):
+        for j in range(1, n_blocks + 1):
+            if i != j:
+                res.append(['Put on', i, j])
+    return res
+
+
+def evaluate_state(curr, goal):
+    return np.linalg.norm(curr[..., :-1] - goal[..., :-1])
+
+
+class ContinousPlanner:
+    def __init__(self, goal=None, n_blocks=8, max_len=50, eps=1e-3):
+        self.goal = goal
+        self.n_blocks = n_blocks
+        self.action_list = list_action(n_blocks)
+        self.max_len = max_len
+        self.eps = eps
+
+    def set_goal_(self, goal):
+        self.goal = goal
+
+    def naive_bfs(self, state):
+        assert self.goal is not None, "goal mustn't be None for planner"
+        valid_action_list = self.action_list
+        state_queue = queue.Queue()
+        state_queue.put((state, [], evaluate_state(state, self.goal)))
+        while state_queue.qsize() < self.max_len:
+            curr_state, plan, _ = state_queue.get()
+            min_error = 1e10
+            for action in valid_action_list:
+                next_state = apply_action(curr_state, action)
+                error = evaluate_state(next_state, self.goal)
+                state_queue.put((next_state, plan + [action], error))
+                min_error = min(min_error, error)
+            if min_error < self.eps:
+                break
+        selected = sorted(list(state_queue.queue), key=lambda a: a[-1])[0]
+        return selected[1][0]
+
+    def __call__(self, state):
+        return self.naive_bfs(state)
+
+
+CP = ContinousPlanner
+
+class TaskManager:
+    def __init__(self, n_blocks, init_vec, goal_vec, sgn):
+        self.n_blocks = n_blocks
+        self.init_vec = init_vec
+        self.goal_vec = goal_vec
+        self.planner = CP(n_blocks)
+        self.env = BlockStackingEnv(n_blocks)
+        self.env.set_vector_state(init_vec)
+        self.planner.set_goal_(goal_vec)
+        self.sgn = sgn
+
+    def run_planner(self, verbose=False, max_iter=50):
+        iter_count = 0
+        while not np.allclose(self.env.vector_state, self.goal_vec):
+            iter_count = iter_count + 1
+            if iter_count >= max_iter:
+                if verbose: print('exceed max iter, task failed')
+                return 0
+            gt_block_coord = self.env.get_coords_3d()
+            with torch.no_grad():
+                pred_vector = self.sgn(torch.from_numpy(gt_block_coord.flatten()).to(torch.float)).numpy().reshape(self.n_blocks, -1)
+            if verbose: print('predicted vector state\n', pred_vector.round(2))
+            if verbose: print('gt vector state\n', self.env.vector_state)
+            action = self.planner(pred_vector)
+            if not self.env.execute_action(action):
+                if verbose: print('invalid action: {}'.format(action))
+                if verbose: print(self.env.image_state)
+                return 0
+            if verbose: print('executed action {}'.format(action))
+            if verbose: print(self.env.image_state)
+            if verbose: print(self.env.vector_state)
+        if verbose: print('task succeed')
+        return 1
+
+
+if __name__ == '__main__':
+    init_img = np.array(
+        [
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 2, 3, 4, 5, 6, 7, 8],
+        ]
+    )
+    goal_img = np.array(
+        [
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [7, 8, 0, 0, 0, 0, 0, 0],
+            [4, 5, 6, 0, 0, 0, 0, 0],
+            [1, 2, 3, 0, 0, 0, 0, 0],
+        ]
+    )
+    env = BlockStackingEnv(8)
+    sgn = BlockStackingSGN(8, 128, 2)
+    sgn.load_state_dict(torch.load('D:\\CS\\CoRe\\ours\\Probabilistic-PDDL-Solver-CoRe-24Fall\\checkpoints\\sgn-20250103-130556\\model_100.pth'))
+    manager = TaskManager(8, env.image2vector(init_img), env.image2vector(goal_img), sgn)
+    manager.run_planner(verbose=True)
